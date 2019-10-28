b74e6c18d3a6b594d937fdf5e71b4d788ce6f88f
bluepropane <rbluepropane@gmail.com>
2019-10-29 00:26:19 +0800

    feat: Use tapAsync; allow spcifying of context path

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ const MATCH_ENDING_JS = /(.js)$/;
 class JSOutputFilePlugin {
   constructor(options) {
     this.sourceFile = options.sourceFile;
+    this.contextPath = options.contextPath;
     this.outputFileName =
       options.outputFileName || this.sourceFile.replace(MATCH_ENDING_JS, '');
   }
   apply(compiler) {
-    compiler.hooks.emit.tapAsync(
-      'JSOutputFilePlugin',
-      async (compilation, callback) => {
-        const exported = require(path.join(compiler.context, this.sourceFile));
-        let payload = exported;
-        if (typeof exported === 'function') {
-          payload = await exported(compiler.options.mode);
-        }
-        if (typeof payload !== 'string') {
-          payload = JSON.stringify(payload);
-        }
-
-        compilation.assets[this.outputFileName] = {
-          source: () => payload,
-          size: () => payload.length
-        };
-        callback();
+    compiler.hooks.emit.tapPromise('JSOutputFilePlugin', async compilation => {
+      const exported = require(path.join(
+        this.contextPath || compiler.context,
+        this.sourceFile
+      ));
+      let payload = exported;
+      if (typeof exported === 'function') {
+        payload = await exported(compiler.options.mode);
       }
-    );
+      if (typeof payload !== 'string') {
+        payload = JSON.stringify(payload);
+      }
+
+      compilation.assets[this.outputFileName] = {
+        source: () => payload,
+        size: () => payload.length
+      };
+    });
   }
 }
 
